c52b1334e1ae0b7abd576632a5fcf8ff65c1a4e6
Stepanishin <stepanishin92@mail.ru>
2019-02-17 16:25:05 +0500

    Layout for 320-390 px

---
+++ pages/index.vue
@@ -0,0 +1,105 @@
+<template>
+<div>
+   <header>
+        <div class="container">
+            <nav>
+                <span class="header__nav_openMenu"></span>
+                    <p class="header__nav_title">Любитель тестов</p>
+                        <img src="~assets/img/group-13.svg" alt="" class="header__nav_avatare">
+            </nav>
+        </div>
+    </header>
+
+    <section class="reposte">
+        <div class="container container__reposte">
+            <p class="reposte_description">Расскажи о своем участии 
+                                        в проекте: получи +150 баллов 
+                                        и первое достижение!</p>
+        </div>
+    </section>
+
+        <section class="offerFromAdidas">
+        <div class="container container_offerFromAdidas">
+                <h2 class="offerFromAdidas_title">Сходи на тренировку
+                в корнер Adidas!</h2>
+                <p class="offerFromAdidas_address">Мега Белая Дача</p>
+                <p class="offerFromAdidas_data">18 марта, 18:00</p>
+        </div>
+    </section> 
+
+    <section class="categories">
+        <h2 class="categories_title">Выбери категорию, чтобы перейти 
+                к заданиям</h2>
+        <div class="container container__categories">
+            <div class="container__categories_form container__categories_first">
+                <span class="container__categories_title">На стиле</span>
+            </div>
+            <div class="container__categories_form container__categories_second">
+                <span class="container__categories_title">Жить активнее</span>
+            </div>
+            <div class="container__categories_form container__categories_third">
+                <span class="container__categories_title">Просто и красиво</span>
+            </div>
+            <div class="container__categories_form container__categories_fourth">
+                <span class="container__categories_title">Хюгге</span>
+            </div>
+            <div class="container__categories_form container__categories_fifth">
+                <span class="container__categories_title">Время — детям</span>
+            </div>
+        </div>
+    </section>
+
+    <section class="stock">
+        <div class="container">
+            <h2 class="stock_title">Меняй баллы на призы!</h2>
+        </div>
+        <div class="container container__stock">
+            <a href="#" class="container__stock_choose container__stock_reserve">Зарезервировано</a>
+            <a href="#" class="container__stock_choose container__stock_available">Доступные призы</a>
+        </div>
+        <div class="container container__content_first">
+            <p class="container__content_item">Аксессуар</p>
+            <p class="container__content_name">LADY COLLECTION</p>
+            <a href="#" class="container__content_link">Забрать в магазине</a>
+        </div>
+        <div class="container container__content_second">
+            <p class="container__content_item">Кроссовки</p>
+            <p class="container__content_name">STREET BEAT</p>
+            <a href="#" class="container__content_link">Забрать в магазине</a>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <p class="footer_title">Больше баллов и призов — в приложении!</p>
+        </div>
+        <div class="container container__footer">
+            <a href="#"><img src="~assets/img/bitmap-copy-6.png" alt=""></a>
+            <a href="#"><img src="~assets/img/bitmap-copy-5.png" alt=""></a>
+        </div>
+        <div class="container container__footer">
+            <a href="#"><img src="~assets/img/bitmap-copy.png" alt=""></a>
+            <a href="#"><img src="~assets/img/bitmap-copy-2.png" alt=""></a>
+            <a href="#"><img src="~assets/img/bitmap-copy-3.png" alt=""></a>
+            <a href="#"><img src="~assets/img/bitmap-copy-4.png" alt=""></a>
+        </div>
+        <div class="container footer__info">
+            <a href="#" class="footer__info_rules">Правила акции</a>
+            <a href="#" class="footer__info_rules">Юридическая информация</a>
+        </div>
+        <div class="container">
+            <a href="#"  class="footer_redaction"><p>© 2010–2017 «ИКЕА Сентерс Рус Менеджмент»</p></a>
+        </div>
+    </footer>
+</div>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
+
